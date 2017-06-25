3c3bc1a1d946ca9e6ef389bf70f08b85bf52b93e
Andrico <andrico1234@yahoo.co.uk>
2017-06-25 16:51:44 +0100

    add mongoose functionality to project

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var path = require('path');
+
+require('./app_server/models/db');
 
 var index = require('./app_server/routes/index');
 var users = require('./app_server/routes/users');
---
+++ app_server/models/db.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const PORT = 27017;
+
+mongoose.connect(`mongodb://localhost:${PORT}/cafecrawl`);
+
+mongoose.connection.on('connected', () => {
+
+    console.log(`Mongoose successfully connected to port ${PORT}`);
+});
+
+mongoose.connection.on('error', (err) => {
+
+    console.log(`Mongoose connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+
+    console.log('Mongoose disconnected');
+});
+
+var gracefulExit = (message) => {
+
+    return new Promise((res) => {
+
+        console.log(`Mongoose disconnected through ${message}`);
+        res();
+    });
+};
+
+process.once('SIGUSR2', () => {
+
+    gracefulExit('nodemon restart').then(() => {
+
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', () => {
+
+    gracefulExit('hello').then(() => {
+
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', () => {
+
+    gracefulExit('Heroku App Shutdown').then(() => {
+
+        process.exit(0);
+    });
+});
